35aa31c06ee9a3aa4f6d189b4ad6c67d4ec13cb8
Isaac Karanja <55271909+zak-rockerfeller@users.noreply.github.com>
2022-10-08 07:19:14 +0300

    Update uno.ino

--- uno/uno.ino
+++ uno/uno.ino
@@ -1,7 +1,7 @@
 //This is an IoT project for an automated irrigation and farm monitoring system. Farming parameters such as soil moisture, humidity and temperature are sent to a Raspberry pi server which handles
 //the automation of the water pump. Data is also sent to ThingSpeak, an IoT analytics platform service that allows you to aggregate, visualize, and analyze live data streams in the cloud. 
 
-//It makes use of the ESP8266 development board,DHT sensor and a soil moisture sensor. We will use MQQT protocol to send data to the raspberry pi. MQTT is a lightweight, publish-subscribe, machine to machine network protocol.
+//It makes use of the ESP8266 development board,DHT sensor and a soil moisture sensor. We will use an MQTT protocol to send data to the raspberry pi. MQTT is a lightweight, publish-subscribe, machine to machine network protocol.
 
 
 // Import required libraries
